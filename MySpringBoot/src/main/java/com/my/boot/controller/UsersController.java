139f42f6fbd38a35d6ff0c162e467506370bed25
itswwl <2436951731@qq.com>
2017-07-09 09:39:35 +0800

    server port

--- MySpringBoot/src/main/java/com/my/boot/controller/UsersController.java
+++ MySpringBoot/src/main/java/com/my/boot/controller/UsersController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.my.boot.model.Users;
 import com.my.boot.service.UsersService;
@@ -24,6 +25,12 @@ public class UsersController {
 		List<Users> users =  usersService.userList();
 		model.addAttribute("users", users);
 		return "/users/users";
+	}
+	
+	@ResponseBody
+	@RequestMapping("test")
+	public String test(){
+		return "test";
 	}
 
 }
